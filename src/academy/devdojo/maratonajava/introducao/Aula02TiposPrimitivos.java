07a5ba60fbb79d827b26504aa7ea9a5828efb6c0
Nickolas Aranha <aranha.nickolas@gmail.com>
2025-03-11 17:26:16 -0300

    Arquivos iniciais de Estudo

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        char initial = 'N';
+        int age = 18;
+        long numeroExtenso = 10000;
+        double salary = 28.0;
+        float tax = 20;
+        byte agebyte = 10;
+        short ageShort = 10;
+        boolean bornInBrazil = true;
+
+
+        System.out.println("A inicial do empregado é " + initial + ", ele tem " + age + ", seu salário bruto é de " + salary + " e " + (salary - tax) + " líquido.");
+        // ou //
+
+        System.out.println("A inicial é " + initial);
+        System.out.println("A idade é " + age + " anos");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
