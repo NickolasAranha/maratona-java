636c8233c483703e884d6b6be968aab7b442143d
Nickolas Aranha <aranha.nickolas@gmail.com>
2025-03-31 22:03:47 -0300

    feat: Aulas Atrasadas + Aulas 31/03

--- src/nickolasaranha/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/nickolasaranha/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,17 +1,18 @@
 package nickolasaranha.maratonajava.introducao;
-
+import java.util.Scanner;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
 
         char initial = 'N';
-        int age = 18;
-        long numeroExtenso = 10000;
-        double salary = 28.0;
-        float tax = 20;
+        int age = (int) 18000000000L;
+        long numeroExtenso = 10000L;
+        double tax = 20.00F;
+        float salary = (float) 2500.0D;
         byte agebyte = 10;
         short ageShort = 10;
         boolean bornInBrazil = true;
+        String nome = "Goku";
 
 
         System.out.println("A inicial do empregado é " + initial + ", ele tem " + age + ", seu salário bruto é de " + salary + " e " + (salary - tax) + " líquido.");
@@ -19,9 +20,7 @@ public class Aula02TiposPrimitivos {
 
         System.out.println("A inicial é " + initial);
         System.out.println("A idade é " + age + " anos");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("Oi meu nome é " + nome);
 
 
 
---
+++ src/nickolasaranha/maratonajava/introducao/Aula05OperadorTernario.java
@@ -0,0 +1,11 @@
+package nickolasaranha.maratonajava.introducao;
+
+public class Aula05OperadorTernario {
+    public static void main(String[] args) {
+        double salario = 5000;
+        String mensagemDoar = "";
+        String mensagemNaoDoar = "Não tenho dinheiro agora, mas vou conseguir!";
+        // condicao ? verdadeiro : falso
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : mensagemNaoDoar;
+    }
+}
